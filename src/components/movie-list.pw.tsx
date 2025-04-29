c13f1650a6801e1b361802bf1cf4792c7fffb4ff
Raulif <raul4cade@gmail.com>
2025-04-29 10:19:19 +0200

    Add movie-list tests

---
+++ src/components/movie-list.pw.tsx
@@ -0,0 +1,46 @@
+import {test, expect} from '@playwright/experimental-ct-react'
+import MovieList from './movie-list'
+import {generateMovie} from '@support/factories'
+import sinon from 'sinon'
+import {page} from '@vitest/browser/context'
+
+test.describe('<MovieList>', () => {
+  const sandbox = sinon.createSandbox()
+  const onDelete = sandbox.stub()
+
+  test.afterEach(() => sandbox.restore())
+
+  test('should show nothing with no movies', async ({mount}) => {
+    const component = await mount(<MovieList movies={[]} onDelete={onDelete} />)
+
+    await expect(component.getByTestId('movie-list-comp')).not.toBeVisible()
+  })
+
+  test('should show error with error', async ({mount}) => {
+    const component = await mount(
+      <MovieList movies={{error: 'error'}} onDelete={onDelete} />,
+    )
+    await expect(component.getByTestId('error')).toBeVisible()
+    await expect(component.getByTestId('movie-list-comp')).not.toBeVisible()
+  })
+
+  test('should verify the movie and delete', async ({page, mount}) => {
+    const movie1 = {id: 1, ...generateMovie()}
+    const movie2 = {id: 2, ...generateMovie()}
+    const component = await mount(
+      <MovieList movies={[movie1, movie2]} onDelete={onDelete} />,
+    )
+
+    await expect(component.getByTestId('movie-list-comp')).toBeVisible()
+
+    const listItems = await component.getByTestId('movie-item-comp').all()
+    await expect(listItems).toHaveLength(2)
+    for (const item of listItems) {
+      await expect(item).toBeVisible()
+    }
+
+    await component.getByText('Delete').first().click()
+    await expect(onDelete.calledOnce).toBe(true)
+    await expect(onDelete.calledWith(1)).toBe(true)
+  })
+})
---
+++ src/components/movie-list.vitest.tsx
@@ -0,0 +1,36 @@
+import {
+  describe,
+  expect,
+  it,
+  screen,
+  vi,
+  wrappedRender,
+} from '@vitest-utils/utils'
+import MovieList from './movie-list'
+import {generateMovie} from '@cypress/support/factories'
+
+describe('<MovieList>', () => {
+  const onDelete = vi.fn()
+  const movie1 = {id: 1, ...generateMovie()}
+  const movie2 = {id: 2, ...generateMovie()}
+  it('should show nothing with no movies', () => {
+    wrappedRender(<MovieList movies={[]} onDelete={onDelete} />)
+    expect(screen.queryByTestId('movie-list-comp')).not.toBeInTheDocument()
+  })
+
+  it('should show error with error', () => {
+    wrappedRender(<MovieList movies={{error: 'error'}} onDelete={onDelete} />)
+    expect(screen.queryByTestId('movie-list-comp')).not.toBeInTheDocument()
+    expect(screen.getByTestId('error')).toBeInTheDocument()
+  })
+
+  it('should verify the movie and delete', () => {
+    wrappedRender(<MovieList movies={[movie1, movie2]} onDelete={onDelete} />)
+    expect(screen.getByTestId('movie-list-comp')).toBeVisible()
+    const movieItems = screen.getAllByTestId('movie-item-comp')
+    expect(movieItems).toHaveLength(2)
+    movieItems.forEach((mi) => expect(mi).toBeVisible())
+    screen.getAllByText('Delete')[0]?.click()
+    expect(onDelete).toHaveBeenCalledOnce
+  })
+})
